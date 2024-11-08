70ee2b0d09158f2d872ad8caccb12900682b215b
Guilherme Holi <guilhermeholi09@gmail.com>
2024-11-07 22:18:59 -0300

    Lampada

--- app.js
+++ app.js
@@ -3,12 +3,34 @@
  * @author Guilherme Holi
  */
 
+let chave = false
+let lampada = true
+
+
 function quebrar() {
-    document.getElementById('lamp').src="img/broken.jpg"
-    //reproduzindo um arquivo de audio do JS
-    // passo 1: copiar o arquivo de audio para o projeto
-    // passo 2: usae a classe Audio(biblioteca interna do JS)
-    let som =  new Audio()
-    som.src= "sound/glassbreaking.wav"
-    som.play()
+    if (lampada === true) {
+        //reproduzindo um arquivo de audio do JS
+        // passo 1: copiar o arquivo de audio para o projeto
+        // passo 2: usae a classe Audio(biblioteca interna do JS)
+        let som = new Audio()
+        som.src = "sound/glassbreaking.wav"
+        som.play()
+        //apoio a logica para o JS identificar a lampada
+        document.getElementById('lamp').src = "img/broken.jpg"
+        lampada = false
+    }
 }
+
+function onoff() {
+    if (chave === false && lampada === true) {
+        document.getElementById('interruptor').src = "img/swon.png"
+        document.getElementById('lamp').src = "img/on.jpg"
+        chave = true //chave ligada
+    } else if (lampada === true) {
+        document.getElementById('interruptor').src = "img/swoff.png"
+        document.getElementById('lamp').src = "img/off.jpg"
+        chave = false
+    }
+}
+
+
